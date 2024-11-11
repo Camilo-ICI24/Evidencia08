67461710a9dfdbb5606e533ca2fdc6daffb1c4c8
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-11-10 23:27:03 -0300

    Actualización: corrección de clase Hoja

--- Codigo2/src/main/java/Hoja.java
+++ Codigo2/src/main/java/Hoja.java
@@ -6,53 +6,50 @@ public class Hoja {
 	private String estadoHoja;
 
 	public String getApunte() {
+
 		return this.apunte;
 	}
-
-	/**
-	 * 
-	 * @param apunte
-	 */
 	public void setApunte(String apunte) {
+
 		this.apunte = apunte;
 	}
 
 	public int getNumero() {
+
 		return this.numero;
 	}
 
-	/**
-	 * 
-	 * @param numero
-	 */
 	public void setNumero(int numero) {
+
 		this.numero = numero;
 	}
 
 	public String getEstadoHoja() {
+
 		return this.estadoHoja;
 	}
 
-	/**
-	 * 
-	 * @param estadoHoja
-	 */
 	public void setEstadoHoja(String estadoHoja) {
+
 		this.estadoHoja = estadoHoja;
 	}
 
-	/**
-	 * 
-	 * @param apunte
-	 */
+
 	public boolean apuntar(String apunte) {
-		// TODO - implement Hoja.apuntar
-		throw new UnsupportedOperationException();
+		if (estadoHoja.equals("Arrancada")) {
+			return false;
+		}
+		this.apunte = apunte;
+		return true;
 	}
 
 	public boolean arrancarHoja() {
-		// TODO - implement Hoja.arrancarHoja
-		throw new UnsupportedOperationException();
+		if (estadoHoja.equals("Arrancada")) {
+			return false;
+		}
+		this.estadoHoja = "Arrancada";
+		this.apunte = null;
+		return true;
 	}
 
 }
